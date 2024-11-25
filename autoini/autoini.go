5032a4c605410cb343b5f1dd8ac6d23b86088a1f
Ihor Sikachyna <ihor.sikachyna@gmail.com>
2024-11-25 12:17:17 -0500

    Add tests to autoini package and update its logic (#3)
    * Start implementing autoini tests, return errors instead of failing
    
    * Make ini parser setters use a generic method, check for exported Configurable type fields
    
    * Simplified setGenericKey interface, updated tests

--- autoini/autoini.go
+++ autoini/autoini.go
@@ -1,12 +1,16 @@
 package autoini
 
 import (
-	"gopkg.in/ini.v1"
-	"log"
+	"errors"
 	"reflect"
+	"unicode"
+
+	"gopkg.in/ini.v1"
 )
 
-type Configurable interface {
+type Configurable interface{}
+
+type ImplementsOptional interface {
 	Optional(key string) bool
 }
 
@@ -24,86 +28,136 @@ type ImplementsDefaultBool interface {
 
 type ImplementsPostInit interface {
 	// Must be implemented as a pointer receiver
-	PostInit()
+	PostInit() (err error)
 }
 
-func getKey(key string, optional bool, cfg *ini.File) (valueInConfig *ini.Key) {
+type implementationChecker interface{}
+
+func isOptional[T Configurable](result T, key string) bool {
+	if def, ok := any(result).(ImplementsOptional); ok {
+		return def.Optional(key)
+	}
+	return false
+}
+
+func getKey(key string, optional bool, cfg *ini.File) (valueInConfig *ini.Key, err error) {
 	valueExists := cfg.Section("").HasKey(key)
 	if !valueExists {
 		if !optional {
-			log.Fatalln("Non-optional config file key not found: ", key)
+			return valueInConfig, errors.New("non-optional config file key not found: " + key)
 		}
 	} else {
 		valueInConfig = cfg.Section("").Key(key)
 	}
-	return valueInConfig
+	return
 }
 
-func setStringKey[T Configurable](result T, key string, value reflect.Value, cfg *ini.File) {
-	valueInConfig := getKey(key, result.Optional(key), cfg)
-	if valueInConfig != nil {
-		value.SetString(valueInConfig.String())
-	} else if def, ok := any(result).(ImplementsDefaultString); ok {
-		value.SetString(def.DefaultString(key))
-	}
-}
+type Setter func(value reflect.Value, valueInConfig *ini.Key) (err error)
+type DefaultHandler[Implementation implementationChecker] func(def Implementation, key string, value reflect.Value) (err error)
 
-func setIntKey[T Configurable](result T, key string, value reflect.Value, cfg *ini.File) {
-	valueInConfig := getKey(key, result.Optional(key), cfg)
-	if valueInConfig != nil {
-		val, err := valueInConfig.Int64()
-		if err != nil {
-			log.Fatalf("Config file key %v is not an int", key)
-		}
-		value.SetInt(val)
-	} else if def, ok := any(result).(ImplementsDefaultInt); ok {
-		value.SetInt(int64(def.DefaultInt(key)))
-	}
-}
-
-func setBoolKey[T Configurable](result T, key string, value reflect.Value, cfg *ini.File) {
-	valueInConfig := getKey(key, result.Optional(key), cfg)
-	if valueInConfig != nil {
-		val, err := valueInConfig.Bool()
-		if err != nil {
-			log.Fatalf("Config file key %v is not a bool", key)
-		}
-		value.SetBool(val)
-	} else if def, ok := any(result).(ImplementsDefaultBool); ok {
-		value.SetBool(def.DefaultBool(key))
-	}
-}
-
-func ReadIni[T Configurable](path string) (result T) {
-	cfg, err := ini.Load(path)
-	// TODO: return an error instead of exiting
+func setGenericKey[T Configurable, Implementation implementationChecker](result T, key string, value reflect.Value, cfg *ini.File, setter Setter, defaultHandler DefaultHandler[Implementation]) (err error) {
+	valueInConfig, err := getKey(key, isOptional(result, key), cfg)
 	if err != nil {
-		log.Fatalf("Fail to read file: %v", err)
+		return err
+	}
+
+	if valueInConfig != nil {
+		err = setter(value, valueInConfig)
+	} else if def, ok := any(result).(Implementation); ok {
+		err = defaultHandler(def, key, value)
+	} else {
+		return errors.New("did not find a value or default value for key: " + key)
+	}
+	return
+}
+
+func setStringKey[T Configurable](result T, key string, value reflect.Value, cfg *ini.File) (err error) {
+	return setGenericKey(result, key, value, cfg,
+		func(value reflect.Value, valueInConfig *ini.Key) (err error) {
+			value.SetString(valueInConfig.String())
+			return
+		},
+		func(def ImplementsDefaultString, key string, value reflect.Value) (err error) {
+			value.SetString(def.DefaultString(key))
+			return
+		},
+	)
+}
+
+func setIntKey[T Configurable](result T, key string, value reflect.Value, cfg *ini.File) (err error) {
+	return setGenericKey(result, key, value, cfg,
+		func(value reflect.Value, valueInConfig *ini.Key) (err error) {
+			val, err := valueInConfig.Int64()
+			if err != nil {
+				return errors.New("config file key " + key + " is not an int")
+			}
+			value.SetInt(val)
+			return
+		},
+		func(def ImplementsDefaultInt, key string, value reflect.Value) (err error) {
+			value.SetInt(int64(def.DefaultInt(key)))
+			return
+		},
+	)
+}
+
+func setBoolKey[T Configurable](result T, key string, value reflect.Value, cfg *ini.File) (err error) {
+	return setGenericKey(result, key, value, cfg,
+		func(value reflect.Value, valueInConfig *ini.Key) (err error) {
+			val, err := valueInConfig.Bool()
+			if err != nil {
+				return errors.New("config file key " + key + " is not a bool")
+			}
+			value.SetBool(val)
+			return
+		},
+		func(def ImplementsDefaultBool, key string, value reflect.Value) (err error) {
+			value.SetBool(def.DefaultBool(key))
+			return
+		},
+	)
+}
+
+func ReadIni[T Configurable](path string) (result T, err error) {
+	cfg, err := ini.Load(path)
+	if err != nil {
+		return result, err
 	}
 
 	configReflection := reflect.ValueOf(&result)
 	typeOfConfig := reflect.Indirect(configReflection).Type()
 
 	for i := 0; i < reflect.Indirect(configReflection).NumField(); i++ {
-		var configValueType = reflect.ValueOf(reflect.Indirect(configReflection).Field(i).Interface()).Type()
 		var fieldName = typeOfConfig.Field(i).Name
+		if len(fieldName) == 0 || !unicode.IsUpper(rune(fieldName[0])) {
+			return result, errors.New("field does not exist or is not exported: " + fieldName)
+		}
+		var configValueType = reflect.ValueOf(reflect.Indirect(configReflection).Field(i).Interface()).Type()
+
 		switch configValueType.Name() {
 		case "string":
-			setStringKey(result, fieldName, reflect.Indirect(configReflection).Field(i), cfg)
+			err = setStringKey(result, fieldName, reflect.Indirect(configReflection).Field(i), cfg)
 		case "int":
-			setIntKey(result, fieldName, reflect.Indirect(configReflection).Field(i), cfg)
+			err = setIntKey(result, fieldName, reflect.Indirect(configReflection).Field(i), cfg)
 		case "int64":
-			setIntKey(result, fieldName, reflect.Indirect(configReflection).Field(i), cfg)
+			err = setIntKey(result, fieldName, reflect.Indirect(configReflection).Field(i), cfg)
 		case "bool":
-			setBoolKey(result, fieldName, reflect.Indirect(configReflection).Field(i), cfg)
+			err = setBoolKey(result, fieldName, reflect.Indirect(configReflection).Field(i), cfg)
 		default:
-			log.Fatalf("Unsupported ini value type: %v", configValueType.Name())
+			return result, errors.New("unsupported ini value type: " + configValueType.Name())
+		}
+
+		if err != nil {
+			return
 		}
 	}
 
 	// Convert to a pointer to support pointer receivers
 	if def, ok := any(&result).(ImplementsPostInit); ok {
-		def.PostInit()
+		err = def.PostInit()
+		if err != nil {
+			return
+		}
 	}
 
 	return
